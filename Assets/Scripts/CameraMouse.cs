f525c5c301a61c0b62b902772845bd9e35baa2f9
Kim Falck <38192323+sloppydemon@users.noreply.github.com>
2024-05-08 13:45:29 +0200

    now using own photo for puzzle

--- Assets/Scripts/CameraMouse.cs
+++ Assets/Scripts/CameraMouse.cs
@@ -1,9 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class CameraMouse : MonoBehaviour
 {
